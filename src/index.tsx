7540c929ed1aabd76b0b3e17e4dffeaf5abeef05
Fco fabio <franciscofabioinffo@gmail.com>
2025-01-13 13:10:07 -0300

    react

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+// Criação da raiz para renderizar o App
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+root.render(<App />);
---
+++ src/react-dom-client.d.ts
@@ -0,0 +1,4 @@
+declare module "react-dom/client" {
+  import { ReactDOM } from "react-dom";
+  export * from "react-dom";
+}
